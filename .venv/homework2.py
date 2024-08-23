b9b8946fa35749946533453350d1e2a3a6c4b469
egormarik <egormarikpt@gmail.com>
2024-08-23 17:25:20 +0300

    file

---
+++ .venv/homework2.py
@@ -0,0 +1,22 @@
+car_model = "Toyota Corolla"
+car_color = "Red"
+car_year = 2020
+car_doors = 4
+
+car_color = "Blue"
+
+print("Model:", car_model)
+print("Color:", car_color)
+print("Year:", car_year)
+print("Doors:", car_doors)
+
+age_person1 = 25
+age_person2 = 30
+age_person3 = 22
+age_person4 = 27
+age_person5 = 35
+total_people = 5
+
+average_age = (age_person1 + age_person2 + age_person3 + age_person4 + age_person5) / total_people
+
+print("Average Age:", average_age)
