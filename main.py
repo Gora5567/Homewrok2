110269de3e0f1da8a40c65420f74a5d2306912a4
egormarik <egormarikpt@gmail.com>
2024-08-24 15:36:32 +0300

    added helper.py updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import helper
+
+helper.start_task(1)
+
 car_model = "Toyota Corolla"
 car_color = "Red"
 car_year = 2020
 car_doors = 4
-
 car_color = "Blue"
-
 print("Model:", car_model)
 print("Color:", car_color)
 print("Year:", car_year)
 print("Doors:", car_doors)
+
+helper.end_task(1)
+
+helper.start_task(2)
 
 age_person1 = 25
 age_person2 = 30
@@ -16,7 +22,7 @@ age_person3 = 22
 age_person4 = 27
 age_person5 = 35
 total_people = 5
-
 average_age = (age_person1 + age_person2 + age_person3 + age_person4 + age_person5) / total_people
-
 print("Average Age:", average_age)
+
+helper.end_task(2)
